b2d3f533f4b3cf82eb7fe2538af2672bda596258
Thomas Albrecht <albrecht.thomas@gmail.com>
2015-09-17 22:45:41 +0200

    Created ILogger

---
+++ IoC-Learning/IoC-Learning/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoC_Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new FileLogger();
+            logger.OpenLog();
+            logger.Log("Hello World!");
+            logger.Log("Just another log message");
+            logger.CloseLog();
+        }
+    }
+}
