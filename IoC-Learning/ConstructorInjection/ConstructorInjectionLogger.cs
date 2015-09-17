9ae291922bbe0a6e86e38aa3b73d34604e4682b2
Thomas Albrecht <albrecht.thomas@gmail.com>
2015-09-17 22:54:21 +0200

    Added comments to constructor injection.

--- IoC-Learning/ConstructorInjection/ConstructorInjectionLogger.cs
+++ IoC-Learning/ConstructorInjection/ConstructorInjectionLogger.cs
@@ -4,8 +4,15 @@ namespace ConstructorInjection
 {
     internal class ConstructorInjectionLogger
     {
+        /// <summary>
+        /// The logger
+        /// </summary>
         private ILogger logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConstructorInjectionLogger"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
         public ConstructorInjectionLogger(ILogger logger)
         {
             this.logger = logger;
--- IoC-Learning/ConstructorInjection/LoggingEngine.cs
+++ IoC-Learning/ConstructorInjection/LoggingEngine.cs
@@ -4,13 +4,24 @@ namespace ConstructorInjection
 {
     internal class LoggingEngine
     {
+        /// <summary>
+        /// The logger
+        /// </summary>
         private ILogger logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggingEngine"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
         public LoggingEngine(ILogger logger)
         {
             this.logger = logger;
         }
 
+        /// <summary>
+        /// Logs the specified log message.
+        /// </summary>
+        /// <param name="logMessage">The log message.</param>
         public void Log(string logMessage)
         {
             this.logger.OpenLog();
