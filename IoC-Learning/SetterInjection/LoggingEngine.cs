8fb5b9818f755c9f85d90d8a276533fc40b06e53
Thomas Albrecht <albrecht.thomas@gmail.com>
2015-09-17 23:07:24 +0200

    Added SetterInjection project

---
+++ IoC-Learning/SetterInjection/LoggingEngine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Interfaces;
+
+namespace SetterInjection
+{
+    internal class LoggingEngine
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LoggingEngine"/> class.
+        /// </summary>
+        public LoggingEngine() { }
+
+        /// <summary>
+        /// Gets or sets the logger.
+        /// </summary>
+        /// <value>
+        /// The logger.
+        /// </value>
+        public ILogger Logger { get; internal set; }
+
+        /// <summary>
+        /// Logs the specified log message.
+        /// </summary>
+        /// <param name="logMessage">The log message.</param>
+        internal void Log(string logMessage)
+        {
+            Logger.OpenLog();
+            Logger.Log(logMessage);
+            Logger.CloseLog();
+        }
+    }
+}
---
+++ IoC-Learning/SetterInjection/Program.cs
@@ -0,0 +1,20 @@
+﻿using Interfaces;
+using IoC_Learning;
+
+namespace SetterInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new FileLogger();
+
+            var setterInjectionLogger = new LoggingEngine();
+
+            setterInjectionLogger.Logger = logger;
+            setterInjectionLogger.Log("Setter Injection logging message");
+
+
+        }
+    }
+}
